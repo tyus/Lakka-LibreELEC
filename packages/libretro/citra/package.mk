7e49f6f13bb4af2f0eb7224e95ea329f63c1b387
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-04-17 05:31:30 +0200

    Fix a part of citra build. Still broken.

--- packages/libretro/citra/package.mk
+++ packages/libretro/citra/package.mk
@@ -39,11 +39,16 @@ PKG_CMAKE_OPTS_TARGET="-DENABLE_LIBRETRO=1 \
                        -DENABLE_QT=0 \
                        -DCMAKE_BUILD_TYPE=\"Release\" \
                        -DBOOST_ROOT=$(get_build_dir boost) \
+                       -DENABLE_WEB_SERVICE=0 \
                        -DUSE_SYSTEM_CURL=1 \
                        -DTHREADS_PTHREAD_ARG=OFF \
                        -DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
                        -DCMAKE_VERBOSE_MAKEFILE=1 \
                        --target citra_libretro"
+
+pre_make_target() {
+  find . -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+}
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
